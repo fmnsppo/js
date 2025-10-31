ceef09c23275d84245e52abdedaf0da173cf611b
fmnsppo <119563766+fmnsppo@users.noreply.github.com>
2025-10-31 08:31:08 +0800

    Add files via upload

---
+++ v.js
@@ -0,0 +1,59 @@
+var _0x18465c = (_0x320922) => {
+    if (!_0x320922) {
+        return _0x320922;
+    }
+    try {
+        var _0x712277 = String(_0x320922);
+        var _0x2269b2 = "43e280fe815d80ae8c715a213e6e499c";
+        return ((_0x1b57c5) => {
+            var _0x550509 = ((_0x5cbc31, _0x472413) => {
+                var _0x3dcadd = _0x5cbc31.length;
+                var _0x322bdc = _0x472413.split("");
+                var _0x45cee0 = _0x322bdc.map((_0x2a44fc, _0x2c708e) =>
+                    Math.floor((_0x3dcadd * (5 + 10 * _0x2c708e)) / 100),
+                );
+
+                let _0x11d298 = "";
+                let _0x252794 = 0;
+                _0x45cee0.forEach((_0x18230f, _0x5bf666) => {
+                    if (_0x18230f >= _0x3dcadd) {
+                        return;
+                    }
+                    var _0x24a7af = _0x5cbc31.substring(_0x252794, _0x18230f);
+
+                    _0x11d298 += _0x24a7af + _0x322bdc[_0x5bf666];
+                    _0x252794 = _0x18230f;
+                });
+                _0x11d298 += _0x5cbc31.substring(_0x252794);
+
+                return _0x11d298;
+            })(_0x1b57c5, Math.floor(Date.now() / 1000).toString());
+
+            return ((_0x329b07, _0x2556b7) => {
+                var _0x294b9c = _0x329b07.length;
+                var _0x450125 = [];
+                for (let _0x58c916 = 0; _0x58c916 < _0x2556b7.length; _0x58c916 += 4) {
+                    _0x450125.push(_0x2556b7.substr(_0x58c916, 4));
+                }
+                var _0x7344e7 = [20, 35, 43, 59, 67, 74, 87, 95].map((_0x1c1551) =>
+                    Math.floor((_0x294b9c * _0x1c1551) / 100),
+                );
+                let _0x472580 = "";
+                let _0x4fc7a3 = 0;
+                _0x7344e7.forEach((_0x4b3710, _0x1df095) => {
+                    if (_0x1df095 >= _0x450125.length || _0x4b3710 >= _0x294b9c) {
+                        return;
+                    }
+                    var _0x3085e0 = _0x329b07.substring(_0x4fc7a3, _0x4b3710);
+                    _0x472580 += _0x3085e0 + _0x450125[_0x1df095];
+                    _0x4fc7a3 = _0x4b3710;
+                });
+                _0x472580 += _0x329b07.substring(_0x4fc7a3);
+
+                return _0x472580;
+            })(_0x550509, CryptoJS.MD5(_0x550509).toString());
+        })(CryptoJS.AES.encrypt(_0x712277, _0x2269b2).toString());
+    } catch (_0x387ff5) {
+        return _0x320922;
+    }
+};
