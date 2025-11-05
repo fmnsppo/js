1d27f863b8a067441a16bfa93d14d679d45f3cfd
fmnsppo <119563766+fmnsppo@users.noreply.github.com>
2025-11-05 09:48:49 +0000

    v_2.js

---
+++ v_2.js
@@ -0,0 +1,19 @@
+var tt = {
+    secret_key: "EA^UfBOF9lNdQDS3i2qAnsqxIrTpH%",
+    encrypt_key: "6dFGd4Laa3vE%kLpr5eCtSEaAL%wJm",
+    downloadKey: "7ddFg5%aa3vE%kRpr5eCxQEoALtwJm"
+};
+
+function generateSigned() {
+    secret_key = tt.secret_key;
+    encrypt_key = tt.encrypt_key;
+    download_key = tt.downloadKey;
+    let n = Date.now().toString(),
+        i = {
+            hash: CryptoJS.HmacSHA256(JSON.stringify({
+                timesTamp: n
+            }), this.secret_key).toString(),
+            timesTamp: n
+        };
+    return CryptoJS.AES.encrypt(JSON.stringify(i), this.encrypt_key).toString()
+}
